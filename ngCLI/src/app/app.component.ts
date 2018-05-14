b58bd9807e9f03ecd1fcde6e75aeb36b140ad405
rushi <rushikesh@users.noreply.github.com>
2018-05-14 11:34:26 +0530

    angular cli example code base

---
+++ ngCLI/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+import * as moment from "moment";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title = "Tour Of Heros";
+  constructor() {
+    console.log(moment().toJSON());
+  }
+}
